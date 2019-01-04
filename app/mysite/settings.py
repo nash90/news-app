756140d2c034785b349c4bd868fa26d8204ee917
Abinash Adhikari (nash90) <nash.rakuten@gmail.com>
2019-01-04 13:59:40 -0800

    added allowed hosts

--- app/mysite/settings.py
+++ app/mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '%iwveaturqi8lh$u-n526ftr(c2#-d*^p!-l3d^da_2opwilg9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
